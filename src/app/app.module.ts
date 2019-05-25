a80859645efcec2992138883cc9a6c51cf0accb8
Zaky7 <zak.saifi7@gmail.com>
2019-05-25 18:36:10 +0530

    Created ui components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ChatNavBarComponent,
+    ChatMessageComponent,
+    ChatPageComponent,
+    ChatThreadComponent,
+    ChatThreadsComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/thread/thread.service.spec.ts
@@ -0,0 +1,41 @@
+import { User } from '../user/user.model';
+import { Thread } from './thread.model';
+import { Message } from '../message/message.model';
+import { MessagesService } from '../message/messages.service';
+import { ThreadsService } from './thread.service';
+
+import * as _ from '../util/lodash';
+
+describe('ThreadService', () => {
+    const zakir: User = new User('Nate Murray', '');
+    const felipe: User = new User('Felipe Curry', '');
+
+    const t1: Thread = new Thread('t1', 'Thread 1', '');
+    const t2: Thread = new Thread('t2', 'Thread 2', '');
+
+    const m1: Message = new Message({
+        author: zakir,
+        text: 'Hi!',
+        thread: t1
+    });
+
+    const m2: Message = new Message({
+        author: felipe,
+        text: 'Hi! Zakir',
+        thread: t2
+    });
+
+    const messagesService: MessagesService = new MessagesService();
+    const threadsService: ThreadsService = new ThreadsService(messagesService);
+
+    threadsService.threads.subscribe(
+        (threadIdx: { [key: string]: Thread }) => {
+            const threads: Thread[] = _.values(threadIdx);
+            const threadNames: string = _.map(threads, (t: Thread) => t.name).join(', ');
+
+            console.log(`=> threads (${threads.length}): ${threadNames}`);
+        });
+
+    messagesService.addMessage(m1);
+    messagesService.addMessage(m2);
+});
